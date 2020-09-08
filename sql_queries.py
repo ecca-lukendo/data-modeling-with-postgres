ae52dda69d6c6ba9524c756b85cfbb6dc4a57acc
Ecca Lukendo <ecca.lukendo@gmail.com>
2020-09-08 22:32:36 +0200

    Second code review changes

--- sql_queries.py
+++ sql_queries.py
@@ -29,17 +29,16 @@ songplay_table_insert = ("INSERT INTO songplays(start_time, user_id, level, song
 	VALUES (%s, %s, %s, %s, %s, %s, %s, %s);")
 
 user_table_insert = ("INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) \
-	ON CONFLICT(user_id) DO UPDATE SET first_name = EXCLUDED.first_name, last_name = EXCLUDED.last_name, gender = EXCLUDED.gender, level = EXCLUDED.level;")
+	ON CONFLICT(user_id) DO UPDATE SET level = EXCLUDED.level;")
 
 song_table_insert = ("INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) \
-	ON CONFLICT(song_id) DO UPDATE SET title = EXCLUDED.title, artist_id = EXCLUDED.artist_id, year = EXCLUDED.year, duration = EXCLUDED.duration;")
+	ON CONFLICT(song_id) DO NOTHING;")
 
 artist_table_insert = ("INSERT INTO artists(artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) \
-	ON CONFLICT(artist_id) DO UPDATE SET name = EXCLUDED.name, location = EXCLUDED.location, latitude = EXCLUDED.latitude, longitude = EXCLUDED.longitude;")
+	ON CONFLICT(artist_id) DO NOTHING;")
 
 time_table_insert = ("INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) \
-	ON CONFLICT(start_time) DO UPDATE SET hour = EXCLUDED.hour, day = EXCLUDED.day, week = EXCLUDED.week, month = EXCLUDED.month, \
-	year = EXCLUDED.year, weekday = EXCLUDED.weekday;")
+	ON CONFLICT(start_time) DO NOTHING;")
 
 # FIND SONGS
 
